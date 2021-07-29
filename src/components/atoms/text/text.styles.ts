2cfb3aebb3dab0ec51048cff939df0f678314d26
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-07-29 10:02:29 +0200

    repare text style

--- src/components/atoms/text/text.styles.ts
+++ src/components/atoms/text/text.styles.ts
@@ -3,7 +3,8 @@ import { TextProps } from './text.types';
 
 export const TextContainer = styled.span<TextProps>`
   width: 100%;
-  margin: ${({ margin }) => margin + 'px'}${({ marginLeft = 0 }) => marginLeft + 'px'};
+  margin: ${({ margin }) => margin + 'px'};
+  ${({ marginLeft = 0 }) => marginLeft + 'px'};
   font-size: ${({ fontSize, theme }) => theme.fontSize[fontSize]};
   font-family: ${({ specialFont = false, theme }) =>
       specialFont ? theme.fontFamily.specialFont : theme.fontFamily.mainFont},
@@ -17,7 +18,6 @@ export const TextContainer = styled.span<TextProps>`
   @media (max-width: 580px) {
     width: auto;
     display: flex;
-
     font-size: ${({ specialFont, fontSize, theme }) =>
       specialFont ? theme.fontSize['S'] : theme.fontSize[fontSize]};
     margin-left: 5px;
