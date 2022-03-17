f557a15dc44d341e7a18f4057a91b59424a48ac2
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2022-03-17 21:45:54 +0100

    feat: add link

--- src/components/organisms/footer/footer.tsx
+++ src/components/organisms/footer/footer.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react';
-import { Wrapper } from './footer.styles';
+import { Wrapper, FooterLink } from './footer.styles';
 
 const Footer: FC = () => (
-  <Wrapper>© 2021 Mateusz Grzejszczyk. All Rights Reserved</Wrapper>
+  <Wrapper>
+    <FooterLink href="https://mateusz-grzejszczyk-frontend-developer.netlify.app/">
+      © 2021 Mateusz Grzejszczyk. All Rights Reserved
+    </FooterLink>
+  </Wrapper>
 );
 
 export default Footer;
